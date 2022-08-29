9109c54fb553c773230ecdaf21aa8d4dc3c98a80
Iyer <akiyer@deloitte.com>
2022-08-29 12:27:56 +0530

    initial commit

---
+++ HTML-bootstrap-project/table.js
@@ -0,0 +1,105 @@
+const body = document.querySelector("body");
+
+const table = document.createElement('table')
+table.style.border = "1px solid red";
+table.style.borderCollapse = "collapse";
+table.style.width = "80%";
+
+const data = [
+    {
+        "city": "Mumbai",
+    }, {
+        "city": "Chennai",
+    }, {
+        "city": "Bengaluru",
+    }];
+
+
+addHeaderRow();
+createDataRows();
+
+body.appendChild(table)
+
+createButton();
+
+function addHeaderRow() {
+    const headerRow = document.createElement('tr')
+
+    const headerCol1 = document.createElement('th')
+    headerCol1.innerHTML = "CITY"
+    headerCol1.style.border = "1px solid red"
+    headerCol1.style.backgroundColor = "yellow"
+
+    // const headerCol2 = document.createElement('th')
+    // headerCol2.innerHTML = "AGE"
+    // headerCol2.style.border = "1px solid red"
+    // headerCol2.style.backgroundColor = "yellow"
+
+    // const headerCol3 = document.createElement('th')
+    // headerCol3.innerHTML = "SALARY"
+    // headerCol3.style.border = "1px solid red"
+    // headerCol3.style.backgroundColor = "yellow"
+
+    headerRow.appendChild(headerCol1);
+    // headerRow.appendChild(headerCol2);
+    // headerRow.appendChild(headerCol3);
+
+    table.appendChild(headerRow);
+}
+
+function createDataRows() {
+
+    for (emp of data) {
+        const dataRow = document.createElement('tr')
+        dataRow.className = 'dataRow';
+
+        const col1 = document.createElement('td')
+        col1.innerHTML = emp.city;
+        col1.style.border = "1px solid red"
+
+        // const col2 = document.createElement('td')
+        // col2.innerHTML = emp.age;
+        // col2.style.border = "1px solid red"
+
+        // const col3 = document.createElement('td')
+        // col3.innerHTML = emp.salary;
+        // col3.style.border = "1px solid red"
+
+        dataRow.appendChild(col1);
+        // dataRow.appendChild(col2);
+        // dataRow.appendChild(col3);
+
+        table.appendChild(dataRow);
+    }
+
+}
+
+function createButton() {
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Add Row';
+
+    btn.onclick = addRow;
+
+    body.appendChild(document.createElement('br'))
+    body.appendChild(btn)
+}
+
+function addRow() {
+    const obj = {
+        "City": "City_name",
+        
+    }
+
+    clearAllDataRows();
+    data.push(obj);
+    createDataRows();
+
+}
+
+function clearAllDataRows() {
+    const allRows = table.querySelectorAll('.dataRow');
+    for (row of allRows) {
+        table.removeChild(row);
+    }
+}
